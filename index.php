149303443d1d207c2b69e2cb8c69110f898b0a28
Paul <pauljs.go@gmail.com>
2020-07-23 23:32:55 -0500

    Inicio del Proyecto Cajitas de Talentos

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+<title>Las Cajitas de Talentos</title>
+<meta charset="UTF-8">
+<link href="css/base.css" rel="stylesheet">
+
+<meta name="viewport" content="Width=device-width;initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+
+</head>
+<body>
+
+</body>
+
+</html>
