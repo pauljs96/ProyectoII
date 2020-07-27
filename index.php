7dcce847b3837cecdacca9b95e60f5ffeb4d3ca3
Paul <pauljs.go@gmail.com>
2020-07-26 23:23:56 -0500

    Termino del Header y Avance del Main

--- index.php
+++ index.php
@@ -19,17 +19,18 @@
 
 
 <header>
+
 <div class="btn-menu-class" id="btn-menu-class">
     <a href="#" class="btn-menu"><span class="icon-menu"></span>Menú</a>
 </div>
 
 
-<nav class="menu-class" id="menu-class">
+<div class="menu-class" id="menu-class">
     <ul>
         <li><a href="#"><span class="icon-accessibility"></span>¿Qué es esto?</a></li>
         <li><a href="#"><span class="icon-gift"></span>¿Qué ofrecemos?</a></li>
         <li id="submenu">
-            <a href="#"><span class="icon-images"></span>Experiencias<span class=" flecha icon-arrow-down2"></span></a>
+            <a href="#"><span class="icon-images"></span>Experiencias<span class=" flecha icon-circle-down"></span></a>
             <ul class="children" id="children">
         <li><a href="#">Creativas<span class="icon-star-full"></span></a></li>
         <li><a href="#">Estudiosas<span class="icon-star-full"></span></a></li>
@@ -38,30 +39,72 @@
         </li>
         <li><a href="#"><span class="icon-phone"></span>Contáctanos</a></li>
     </ul>
-</nav>
-<div class="titulo-class"><h1>Cajitas de Talentos</h1></div>
+</div>
+<div class="titulo-class"><img src="img/titulo.png"></div>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br><br>
-<br>
-<br>
-<br>
 
 </header>
 
 
 <main>
 
-<div>hola</div>
-<div>hola</div>
-<div>hola</div>
-<div>hola</div>
+<section class="section1">
+
+<div class="video-class">
+<video src="video/video.mp4" muted autoplay loop></video>
+</div>
+
+
+<div class="textovideo-class">
+<h1>Enfrenta los retos y vence</h1>
+<a href="#" target="_blank" class="enlace-video">Explora</a>
+</div>
+
+</section>
+
+
+<section class="section2">
+
+<h1>Bienvenidos</h1>
+<p>Ahora tendrás la oportunidad de descubrir tus talentos y divertirte. 
+Con las Cajitas de Talentos pueden conseguir inscribirte en actividades que complementan tu día</p>
+
+</section>
+
+<section class="section3">
+
+    <div class="div-titulo"><h1>Las Cajitas de Talentos</h1></div>
+    <div class="div-cajas">
+            <div class="caja"><a href="#"><img src="img/caja.webp"></a><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio:......</p></div></div>
+            <div class="caja"><a href="#"><img src="img/caja.webp"></a><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio:......</p></div></div>
+            <div class="caja"><a href="#"><img src="img/caja.webp"></a><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio:......</p></div></div>
+    </div>
+    <div class="div-contacto">
+
+<p>Selecciona las cajas mas adecuadas para ti</p>
+
+    </div>
+
+</section>
+
+
+<section class="section4">
+
+<img src="img/main.jpg">
+
+</section>
+
+
+<section class="section5">
+
+<a href="#" class="icon icon-facebook" target="_blank"></a>
+<a href="#" class="icon icon-twitter"target="_blank"></a>
+<a href="#" class="icon icon-whatsapp"target="_blank"></a>
+<a href="#" class="icon icon-instagram"target="_blank"></a>
+<a href="#" class="icon icon-youtube"target="_blank"></a>
+</section>
+
+
 </main>
 
 <footer>
