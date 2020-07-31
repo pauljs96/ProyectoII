98ca9f0c699c300cd7845136278984c4d4c69860
Paul <pauljs.go@gmail.com>
2020-07-31 01:52:52 -0500

    Creación de las secciones de la página principal con su respectivo diseño responsive

--- index.php
+++ index.php
@@ -20,11 +20,12 @@
 
 <header>
 
+<!-- Botón menu en dispositivos mini -->
 <div class="btn-menu-class" id="btn-menu-class">
     <a href="#" class="btn-menu"><span class="icon-menu"></span>Menú</a>
 </div>
 
-
+<!-- Menu -->
 <div class="menu-class" id="menu-class">
     <ul>
         <li><a href="#"><span class="icon-accessibility"></span>¿Qué es esto?</a></li>
@@ -40,6 +41,8 @@
         <li><a href="#"><span class="icon-phone"></span>Contáctanos</a></li>
     </ul>
 </div>
+
+<!-- Titulo -->
 <div class="titulo-class"><img src="img/titulo.png"></div>
 
 
@@ -48,6 +51,7 @@
 
 <main>
 
+<!-- Enlace para videos de experiencias anteriores -->
 <section class="section1">
 
 <div class="video-class">
@@ -62,6 +66,7 @@
 
 </section>
 
+<!-- Bienvenido -->
 
 <section class="section2">
 
@@ -71,44 +76,195 @@ Con las Cajitas de Talentos pueden conseguir inscribirte en actividades que comp
 
 </section>
 
+<!-- Vista de las Cajas -->
+
 <section class="section3">
 
     <div class="div-titulo"><h1>Las Cajitas de Talentos</h1></div>
-    <div class="div-cajas">
-            <div class="caja"><a href="#"><img src="img/caja.webp"></a><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio:......</p></div></div>
-            <div class="caja"><a href="#"><img src="img/caja.webp"></a><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio:......</p></div></div>
-            <div class="caja"><a href="#"><img src="img/caja.webp"></a><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio:......</p></div></div>
+    
+    <div class="div-cajas" id="capa-muestra">
+        <div class="detalles-mini" id="detalles-mini">Mostrar detalles</div>  
+            <div class="caja" >
+                <img src="img/caja.webp">
+                <div class="capa">
+                    <p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio: S/.150</p></div>
+                    <a href="#" class="close icon-cart" ></a>
+                </div>
+            <div class="caja"><img src="img/caja.webp"><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio: S/.150</p></div><a href="#" class="close icon-cart"></a></div>
+            <div class="caja"><img src="img/caja.webp"><div class="capa"><p><span class="icon-yelp"></span>contenido:------<br><span class="icon-yelp"></span>precio: S.150</p></div><a href="#" class="close icon-cart"></a></div>
     </div>
+
     <div class="div-contacto">
-
-<p>Selecciona las cajas mas adecuadas para ti</p>
+<p>Mira detalles de cada uno</p>
 
     </div>
 
 </section>
 
+<!-- Caja creativa -->
+<section class="section4-6" >
+<div class="img-fondo">
+    <img src="img/creativa.jpg">
+</div>
 
-<section class="section4">
+<div class="producto">
+<h1>Cajita Creativa</h1>
+<div class="muestra-imagen" id="section4">Muestra imagen</div>
+<fieldset>
+    <Legend>Descripción</Legend>
+<p>Con esta cajita desarrollaras tus habilidades artisicas. 
+Aprenderás de fotografía y pintura de manera práctica y conocerás parte de la historia del Arte.
+Nuestros educadores serán Paolo,Jelly y Zuni.</p>
 
-<img src="img/main.jpg">
+</fieldset>
+<ul>
+    <h2>Incluye</h2>
+<li><img src="img/icono-clase/foto.svg" width="50px">2 clases de Fotografía</li>
+<li><img src="img/icono-clase/pintura.svg" width="50px">2 clases de Pintura</li>
+<li><img src="img/icono-clase/mona-lisa.svg" width="50px">2 clases de Historia del Arte</li>
+
+</ul>
+
+<div class="precio-creativa"> S/.200</div>
+
+</div>
+
+
+<div class="img-producto">
+<img src="img/caja.webp">
+
+</div>
+
+</section>
+
+<br>
+<!-- Caja estudiosa -->
+<section class="section5" >
+<div class="img-fondo">
+    <img src="img/estudiosa.jpg">
+</div>
+
+<div class="producto">
+<h1>Cajita Estudiosa</h1>
+<div class="muestra-imagen" id="section5">Muestra imagen</div>
+<fieldset>
+    <Legend>Descripción</Legend>
+<p>Con esta cajita desarrollaras tus habilidades artisicas. 
+Aprenderás de fotografía y pintura de manera práctica y conocerás parte de la historia del Arte.
+Nuestros educadores serán Paolo,Jelly y Zuni.</p>
+
+</fieldset>
+<ul>
+    <h2>Incluye</h2>
+<li><img src="img/icono-clase/mate.svg" width="50px">2 asesorias de Matemáticas</li>
+<li><img src="img/icono-clase/letras.svg" width="50px">2 asesorias de Letras</li>
+<li><img src="img/icono-clase/ingles.svg" width="50px">2 asesorias de Ingles</li>
+
+</ul>
+
+<div class="precio-creativa"> S/.200</div>
+
+</div>
+
+
+<div class="img-producto">
+<img src="img/caja.webp">
+
+</div>
 
 </section>
 
 
-<section class="section5">
+
+<br>
+
+
+
+
+<!-- Caja deportista -->
+<section class="section4-6" >
+<div class="img-fondo">
+    <img src="img/deportista.jpg">
+</div>
+
+<div class="producto">
+<h1>Cajita Deportista</h1>
+<div class="muestra-imagen" id="section6">Muestra imagen</div>
+<fieldset>
+    <Legend>Descripción</Legend>
+<p>Con esta cajita desarrollaras tus habilidades artisicas. 
+Aprenderás de fotografía y pintura de manera práctica y conocerás parte de la historia del Arte.
+Nuestros educadores serán Paolo,Jelly y Zuni.</p>
+
+</fieldset>
+<ul>
+    <h2>Incluye</h2>
+<li><img src="img/icono-clase/taekwondo.svg" width="50px">3 clases de Taekwondo</li>
+<li><img src="img/icono-clase/ciclista.svg" width="50px">3 clases de Manejo de bicicleta</li>
+<li><img src="img/icono-clase/yoga.svg" width="50px">3 clases de Yoga</li>
+
+</ul>
+
+<div class="precio-creativa"> S/.300</div>
+
+</div>
+
+
+<div class="img-producto">
+<img src="img/caja.webp">
+
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<!-- social bar -->
+<section class="section7">
 
 <a href="#" class="icon icon-facebook" target="_blank"></a>
-<a href="#" class="icon icon-twitter"target="_blank"></a>
-<a href="#" class="icon icon-whatsapp"target="_blank"></a>
-<a href="#" class="icon icon-instagram"target="_blank"></a>
-<a href="#" class="icon icon-youtube"target="_blank"></a>
+<a href="#" class="icon icon-twitter" target="_blank"></a>
+<a href="#" class="icon icon-whatsapp" target="_blank"></a>
+<a href="#" class="icon icon-instagram" target="_blank"></a>
+<a href="#" class="icon icon-youtube" target="_blank"></a>
 </section>
 
 
 </main>
 
+
+
+
+
 <footer>
-<p>bienvenidos a este local </p>
+
+<div class="foot1">
+    <h1>Nuestra Empresa</h1>
+    <ul>
+        <li><a href="#">Sobre Nosotros</a></li>
+        <li><a href="#">Nuestro equipo</a></li>
+        <li><a href="#">Terminos de uso</a></li>
+        <li><a href="#">Politica de Privacidad</a></li>
+    </ul></div>
+<div class="foot2">
+    <h1>Productos</h1>
+    <ul>
+        <li><a href="#">Clases Individuales</a></li>
+        <li><a href="#">Métodos de pago</a></li>
+        <li><a href="#">Preguntas Frecuentes</a></li>
+        <br>
+        
+    </ul>
+
+</div>
+<div class="foot3">
+    <img src="img/marca.png" ></div>
 
 </footer>
 
--- js/main.js
+++ js/main.js
@@ -6,8 +6,12 @@ var divbotonmenu=document.getElementById("btn-menu-class");
 var menu=document.getElementById("menu-class");
 var submenu=document.getElementById("submenu");
 var children=document.getElementById("children");
-
-
+var contaDetalle=1;
+var verDetalle=document.getElementById("detalles-mini");
+var infoDetallada=document.getElementById("capa-muestra").getElementsByClassName("capa");
+var contaImagen=1;
+var verImagen=document.getElementsByClassName("muestra-imagen");
+var imagenDetallada=document.getElementsByClassName("img-producto");
 var activa=function activa(){
 
     if(contador==1){
@@ -37,6 +41,69 @@ children.style.display="none";
 }
 
 submenu.addEventListener("click",activasub);
+
+
+
+
+var detalles=function detalles(){
+
+if (contaDetalle==1){
+infoDetallada[0].style.transform="scale(1)";
+infoDetallada[1].style.transform="scale(1)";
+infoDetallada[2].style.transform="scale(1)";
+contaDetalle=0;
+verDetalle.textContent="Ocultar Detalles";
+
+}else {
+    contaDetalle=1;
+infoDetallada[0].style.transform="scale(0)";
+infoDetallada[1].style.transform="scale(0)";
+infoDetallada[2].style.transform="scale(0)";
+verDetalle.textContent="Mostrar Detalles";
+}
+
+
+}
+
+verDetalle.addEventListener("click",detalles);
+
+
+var imagen=function imagen(e){
+
+if(contaImagen==1){
+    if(e.target.id=="section4"){
+ imagenDetallada[0].style.display="block";
+ verImagen[0].textContent="Ocultar Imagen"; }
+    if(e.target.id=="section5"){
+    imagenDetallada[1].style.display="block";
+    verImagen[1].textContent="Ocultar Imagen";}
+    if(e.target.id=="section6"){
+        imagenDetallada[2].style.display="block";
+        verImagen[2].textContent="Ocultar Imagen";  }
+contaImagen=0;
+
+}else{
+    if(e.target.id=="section4"){
+        imagenDetallada[0].style.display="none";
+        verImagen[0].textContent="Mostrar Imagen";}
+    if(e.target.id=="section5"){
+           imagenDetallada[1].style.display="none";
+           verImagen[1].textContent="Mostrar Imagen";} 
+    if(e.target.id=="section6"){
+               imagenDetallada[2].style.display="none";
+               verImagen[2].textContent="Mostrar Imagen";} 
+ contaImagen=1;
+
+
+}
+
+}
+
+
+verImagen[0].addEventListener("click",imagen);
+verImagen[1].addEventListener("click",imagen);
+verImagen[2].addEventListener("click",imagen);
+
 
 })();
 
